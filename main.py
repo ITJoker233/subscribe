e27c5768413ac6f4b358c14ef1d3c8c9d29b5ce8
ITJoker233 <pengjiewei@zjleiming.cn>
2024-01-09 15:22:46 +0800

    update v2

--- main.py
+++ main.py
@@ -8,44 +8,74 @@ import time
 import json
 import random
 import net
+from fake_useragent import UserAgent
 
 dictKey = '0123456789abcdefghijklmnopqrstuvwxyz'
-
+useragent = UserAgent()
+http = requests.Session()
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.54',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'agentX': 'super-ok-http'
+}
 def getRandomIMEI():
-    return ''.join(random.sample(list(dictKey),16))
+    return 'di:'+''.join(random.sample(list(dictKey),16))
 
-def getSS(url,KEY1,KEY2):
-    v='0.96'
-    imei = 'di:{}'.format(getRandomIMEI())
-    id_ = 6 #小于6的是vip节点
-    #user = random.randint(2800000,2899999)
-    cid = random.randint(0,20) #节点
-    headers = {}
-    postdata = {}
-    postdata['v'] = v
-    postdata['imei'] = imei
-    postdata['id'] = id_
-    postdata['user'] = id_
-    postdata['cid'] = cid
-    headers['User-Agent'] = 'okhttp/4.2.2'
+phone_imei = getRandomIMEI()
+
+def getInfo(KEY1):
+    url = f'{host}/api/user/getInfo'
+    payload = {
+        'a':random.randint(6,14),
+        'b':getRandomIMEI(),
+        'c':''.join(random.sample(list(dictKey),5)),
+        'd':'fuxi',
+        'v':'0.97',
+        'imei':phone_imei,
+    }
+    response = http.post(url,data=payload,headers=headers).text
+    raw_data = base64.b64decode(response)
+    des_obj = des(KEY1, ECB, KEY1, padmode=PAD_PKCS5)
+    data = json.loads(des_obj.decrypt(raw_data).decode())
+    print(data)
+
+def getSS(url,KEY1,KEY2,cid):
+    payload = {
+        'v':0.97,
+        'imei':phone_imei,
+        'id':0,
+        'user':int(time.time()*1000)/10000000,
+        'cid':cid,
+    }
+    headers['User-Agent'] = useragent.random #'okhttp/4.2.2'
     headers['Content-Type'] = 'application/x-www-form-urlencoded'
-    response = requests.post(url,data=postdata,headers=headers).text
+    headers['x-forwarded-for'] = ( str(random.randint(0, 256))+'.'+str(random.randint(0, 256))+'.'+str(random.randint(0, 256))+'.'+str(random.randint(0, 256)) )
+    response = http.post(url,data=payload,headers=headers).text
     raw_data = base64.b64decode(response)
     des_obj = des(KEY2, ECB, KEY2, padmode=PAD_PKCS5)
     data = json.loads(des_obj.decrypt(raw_data).decode())
-    #print('ret:{0}\nlocation:{1}\nping:{2}'.format(data['ret'],data['location'],data['ping']))
     link = base64.b64decode(data['link'])
-    desLink = des(KEY1, ECB, KEY2, padmode=PAD_PKCS5)
-    return desLink.decrypt(link).decode()
+    name = data['location']
+    desLink = des(KEY1, ECB, KEY1, padmode=PAD_PKCS5)
+    try:
+        return name,desLink.decrypt(link).decode()
+    except:
+        return name,None
 
 if __name__ == "__main__":
     if len(sys.argv) > 0:
-        for i in range(5):
-            link = getSS(sys.argv[1],sys.argv[2],sys.argv[3])
+        result = {}
+        for i in range(9):
+            for j in range(10):
+                name,link = getSS(sys.argv[1],sys.argv[2],sys.argv[3],2)
+                if link is not None:
+                    result[name] = link
+        for key in result:
+            link = result[key]
             if net.check(link):
                 with open('latest.txt','w') as f:
                     f.write(base64.b64encode(link.encode()).decode())
-            for file in [x for x in os.listdir() if 'subscribe' in x]:
-                os.remove(file)
-            with open(f'subscribe_{time.strftime("%Y_%m_%d", time.localtime(time.time()))}.txt','w') as f:
-                f.write(base64.b64encode(link.encode()).decode())
+                for file in [x for x in os.listdir() if 'subscribe' in x]:
+                    os.remove(file)
+                with open(f'subscribe_{time.strftime("%Y_%m_%d", time.localtime(time.time()))}.txt','w') as f:
+                    f.write(base64.b64encode(link.encode()).decode())
